7948795916e3ab98ea1f60a8c804e33248f82a3e
eskander-taher <eskander.alshaibany@gmail.com>
2025-08-15 13:24:38 +0300

    update deploy settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Graceful shutdown
 process.on("SIGTERM", () => {
